57fea001061cd6318419d8862dac6c9cbaa31bc8
pitchcontrol <pitchcontrol@yandex.ru>
2015-09-14 17:21:33 +0500

    Первый билд

---
+++ ejbm-ejb/src/main/java/com/mycompany/CurrentTime.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author snekrasov
+ */
+@Stateless
+public class CurrentTime{
+
+     public String GetTime(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
